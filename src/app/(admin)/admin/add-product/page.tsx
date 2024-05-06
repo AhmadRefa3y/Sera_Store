cd3b4f83aa082cfdffddee0899ff5379ae714f38
ahmad <johndoe@example.com>
2024-05-06 22:37:34 +0300

    image Upload

--- src/app/(admin)/admin/add-product/page.tsx
+++ src/app/(admin)/admin/add-product/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+"use client";
+import ImageUpload from "@/components/ImageUpload";
+import React, { useState } from "react";
 
 const page = () => {
-    return <div>page</div>;
+    const [urlImg, seturl] = useState("");
+
+    return (
+        <div>
+            <ImageUpload
+                onChange={(value) => seturl(value)}
+                onRemove={(value) => seturl("")}
+                value={[urlImg]}
+            />
+        </div>
+    );
 };
 
 export default page;
---
+++ src/components/ImageUpload.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { CldUploadWidget } from "next-cloudinary";
+import { useEffect, useState } from "react";
+
+import Image from "next/image";
+import { ImagePlus, Trash } from "lucide-react";
+import { Button } from "./ui/button";
+
+interface imageUploadProps {
+    disabled?: boolean;
+    onChange: (value: string) => void;
+    onRemove: (value: string) => void;
+    value: string[];
+}
+
+const ImageUpload: React.FC<imageUploadProps> = ({
+    disabled,
+    onChange,
+    onRemove,
+    value,
+}) => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    const onUpload = (result: any) => {
+        onChange(result.info.secure_url);
+    };
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <div>
+            <div className="mb-4 flex items-center gap-4">
+                {value.map.length >= 1 &&
+                    value.map(
+                        (url) =>
+                            url.length >= 1 && (
+                                <div
+                                    key={url}
+                                    className="relative w-[200px] h-[200px] rounded-md overflow-hidden]"
+                                >
+                                    <div className="z-10 absolute top-2 right-2">
+                                        <Button
+                                            type="submit"
+                                            onClick={() => onRemove(url)}
+                                            variant={"destructive"}
+                                            size={"icon"}
+                                        >
+                                            <Trash />
+                                        </Button>
+                                    </div>
+                                    <Image
+                                        fill
+                                        className="object-cover"
+                                        alt="iamge"
+                                        src={url}
+                                        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                                    />
+                                </div>
+                            )
+                    )}
+            </div>
+            <CldUploadWidget onSuccess={onUpload} uploadPreset="augvfnz0">
+                {({ open }) => {
+                    const onclick = () => {
+                        open();
+                    };
+                    return (
+                        <Button
+                            type="button"
+                            disabled={disabled}
+                            variant={"secondary"}
+                            onClick={onclick}
+                        >
+                            <ImagePlus className="h-4 w-4 mr-2" />
+                            Upload an Image
+                        </Button>
+                    );
+                }}
+            </CldUploadWidget>
+        </div>
+    );
+};
+export default ImageUpload;

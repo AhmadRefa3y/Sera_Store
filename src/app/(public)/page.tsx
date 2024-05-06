a832f459eb948783b655e79af0d8042c9adf0943
ahmad <johndoe@example.com>
2024-05-06 22:02:48 +0300

    products Grid

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,12 @@
+import ProductsGrid from "@/components/ui/products";
+
+export default function Home() {
+    return (
+        <div>
+            <div className="text-xl font-bold my-2 text-center">
+                اجدد التيشيرتات
+            </div>
+            <ProductsGrid />
+        </div>
+    );
+}

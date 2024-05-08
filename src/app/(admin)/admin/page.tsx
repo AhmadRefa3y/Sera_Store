9f8ffe3b4caf7b2668ad4878ada15116a852eb37
AhmedOmar <ahmedrefay3000@gmail.com>
2024-05-08 11:09:45 +0300

    admin HomePage

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,67 @@
+import {
+    AreaChart,
+    BadgeDollarSign,
+    FileUp,
+    ShoppingBag,
+    TrendingUp,
+} from "lucide-react";
+import React from "react";
+
+const AdminHomePage = () => {
+    return (
+        <div className="flex gap-2 bg-blue-400 h-full p-2">
+            <div className="basis-[60%] bg-green-600 w-full h-full grow flex">
+                <div className="h-[140px] w-full flex gap-1 bg-red-500 p-2">
+                    <div className="flex flex-col gap-4 grow font-bold bg-[#151616] text-muted px-3 text-lg  justify-center rounded-lg">
+                        <div className="flex gap-2">
+                            <AreaChart />
+                            <span className="text-muted font-light">
+                                {" "}
+                                اجمالي المبيعات
+                            </span>{" "}
+                        </div>
+                        <div className="flex gap-2 justify-between ">
+                            <div>5646864.19 $</div>
+                            <div>
+                                <TrendingUp className="rotate-[270]" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 grow font-bold bg-[#151616] text-muted px-3 text-lg  justify-center rounded-lg">
+                        <div className="flex gap-2">
+                            <BadgeDollarSign />
+                            <span className="text-muted font-light">
+                                {" "}
+                                اجمالي الارباح
+                            </span>{" "}
+                        </div>
+                        <div className="flex gap-2 justify-between ">
+                            <div>15732.19 $</div>
+
+                            <div>
+                                <TrendingUp className="rotate-[270]" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-4 grow font-bold bg-[#151616] text-muted px-3 text-lg  justify-center rounded-lg">
+                        <div className="flex gap-2">
+                            <ShoppingBag />
+                            <span className="text-muted font-light">
+                                اجمالي الطلبات
+                            </span>{" "}
+                        </div>
+                        <div className="flex gap-2 justify-between ">
+                            <div>150 الف</div>
+                            <div>
+                                <TrendingUp className="rotate-[270]" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="basis-[40%] bg-yellow-300  w-full h-full grow"></div>
+        </div>
+    );
+};
+
+export default AdminHomePage;
---
+++ src/app/(admin)/admin/products/addProductBtn.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const AddProductBtn = () => {
+    return (
+        <Button
+            className="absolute left-14 top-2 Flex gap-2 "
+            onClick={() => {
+                console.log("clicked");
+
+                redirect("admin/dsa");
+            }}
+        >
+            اضافة منتج
+            <PlusCircle />
+        </Button>
+    );
+};
+
+export default AddProductBtn;
